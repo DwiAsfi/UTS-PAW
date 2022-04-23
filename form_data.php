6ffde8f825f0e88f1c7d88012a8c32702a749174
DwiAsfi <dwiasfi2@gmail.com>
2022-04-23 14:06:43 +0700

    Menambahkan file form_data.php

---
+++ form_data.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>UTS PAW</title>
+</head>
+<body>
+	<div>
+		<h1>Form Tambah Data</h1>
+		<form action="action.php" method="post">
+			<label>Nama : </label>
+			<input type="text" name="nama_121">
+			<br>
+			<label>Email : </label>
+			<input type="text" name="email_121">
+			<br>
+			<input type="submit" value="Tambah Data">
+		</form>
+	</div>
+</body>
+</html>
